2c3777d04ec974f44bafc03848602546f5f7df61
Robbie <robbie_cahill@insprinto.com.au>
2015-06-21 22:09:04 +1000

    Regex and substr functions

--- src/PHPServices/StringOperations.php
+++ src/PHPServices/StringOperations.php
@@ -11,27 +11,49 @@ namespace PHPServices;
 
 class StringOperations
 {
+    /**
+     * @param $string
+     * @return string
+     */
     public function lowercase($string)
     {
         return strtolower($string);
     }
 
+    /**
+     * @param $string
+     * @return string
+     */
     public function firstCharacter($string)
     {
         return $string[0];
     }
 
+    /**
+     * @param $find
+     * @param $replace
+     * @param $string
+     * @return string
+     */
     public function replace($find, $replace, $string)
     {
         $newString = str_replace($find, $replace, $string);
         return $newString;
     }
 
+    /**
+     * @param $needle
+     * @param $haystack
+     * @return string
+     */
     public function inString($needle, $haystack)
     {
         return strstr($haystack, $needle);
     }
 
+    /**
+     * @return string
+     */
     public function randomNumbers()
     {
         $string = '';
@@ -43,6 +65,9 @@ class StringOperations
         return $string;
     }
 
+    /**
+     * @return string
+     */
     public function randomWhitespace()
     {
         $amount = rand(0,10);
@@ -56,6 +81,10 @@ class StringOperations
         return $string;
     }
 
+    /**
+     * Return a not cryptographically secure hash
+     * @return string
+     */
     public function randomHash()
     {
         $string = $this->randomNumbers();
@@ -65,10 +94,55 @@ class StringOperations
         return $string;
     }
 
+    /**
+     * @param $var
+     * @return string
+     */
     public function exportToString($var)
     {
         $exported = var_export($var, true);
         return $exported;
+    }
+
+    /**
+     * @param $pattern
+     * @param $subject
+     * @return array
+     */
+    public function matchRegex($pattern, $subject)
+    {
+        $matches = array();
+
+        preg_match($pattern, $subject, $matches);
+
+        return $matches;
+    }
+
+    /**
+     * Normalized parameters of native PHP function
+     * As with the other string functions in this class
+     * the subject is the last parameter.
+     *
+     * @param $start
+     * @param $length
+     * @param $string
+     *
+     * @return string
+     */
+    public function subString($start, $length, $string)
+    {
+        $subString = substr($string, $start, $length);
+
+        return $subString;
+    }
+
+    /**
+     * @param $string
+     * @return int
+     */
+    public function stringLength($string)
+    {
+        return strlen($string);
     }
 
     public static function __callStatic($name, $arguments)
